b05ceda6d7a42b5132885e386e5b97b76f665702
shota <tontonka2@gmail.com>
2022-03-03 08:42:24 +0900

    add cron

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
+const cron = require('node-cron');
+
+cron.schedule('* * * * * *', () => console.log('毎秒実行'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
